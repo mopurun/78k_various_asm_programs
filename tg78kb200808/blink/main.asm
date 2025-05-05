ab0564ba0195dc529df5870bbc928f692984b44a
もぷるん(mopurun) <174941808+mopurun@users.noreply.github.com>
2025-05-05 12:26:22 +0900

    Create main.asm

---
+++ tg78kb200808/blink/main.asm
@@ -0,0 +1,47 @@
+;making
+;mopurun
+
+
+
+VT	CSEG 	AT 	0H
+	DW	_main,_main
+	
+OP1	CSEG 	AT 	80H
+	DB	6EH,01H,00H,00H,00H
+OP2	CSEG 	AT 	1080H
+	DB	6EH,01H,00H,00H,00H
+	
+	
+C1	CSEG 	AT 	8FH
+_main:
+	;MOV	OSCCTL,#01H
+	MOV	PM6,#0FCH 
+
+loop:
+
+	MOV	P6,#01H  
+	
+	CALL	!delay
+	
+	MOV	P6,#02H 
+	
+	CALL 	!delay
+	
+	BR	$loop
+	
+delay:
+	MOV	A,#0FFH	
+delay_loop1:
+	MOV	X,#0FFH
+delay_loop2:	
+	NOP
+	DEC	X
+	BNZ	$delay_loop2
+	DEC	A	
+	BNZ	$delay_loop1
+	
+	RET
+	
+	
+
+END
